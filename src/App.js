d3145d0932960960b27e5debd97da79ca3e8293f
Andrii <andrii.gorokhovskyi@gmail.com>
2020-07-21 15:41:29 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import "./styles.css";
+import ChatWindow from "./ChatWindow";
+
+const users = [{ username: "Amy" }, { username: "John" }];
+
+const messages = [
+  { username: "Amy", text: "Hi, Jon!" },
+  { username: "Amy", text: "How are you?" },
+  { username: "John", text: "Hi, Amy! Good, you?" }
+];
+
+class AllTogetherExercise2Exercise extends Component {
+  /*
+   If the user did not type anything, he/she should not be
+   allowed to submit.
+   */
+
+  state = {
+    messages: []
+    //messagesEntered: "",
+    //username: ""
+  };
+
+  onAddItem = (username, messagesEntered) => {
+    this.setState({
+      messages: [
+        ...this.state.messages,
+        { username: username, text: messagesEntered }
+      ]
+    });
+  };
+
+  render() {
+    console.log(this.state.messages);
+
+    // let buttonDisabledAmy = true
+    //    if (this.state.messagesAmy) {buttonDisabledAmy=false}
+    let buttonDisabledJohn = false;
+    //  if (this.state.messagesJohn) {buttonDisabledJohn=false}
+
+    return (
+      <div className="container">
+        {users.map((user, id) => (
+          <ChatWindow
+            key={user.username}
+            id={id}
+            messages={this.state.messages}
+            onAddItem={this.onAddItem}
+            users={users}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default AllTogetherExercise2Exercise;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+import AllTogetherExercise2Exercise from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <AllTogetherExercise2Exercise />
+  </React.StrictMode>,
+  rootElement
+);
